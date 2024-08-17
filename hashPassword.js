ff8ca46ef36a9a5777d18b6340a8cb7acf206ac4
rmkofficial <kmert0@outlook.com>
2024-08-17 19:02:58 +0300

    Ignore hashPassword script in .gitignore

--- hashPassword.js
+++ hashPassword.js
@@ -3,9 +3,9 @@ const bcrypt = require('bcryptjs');
 async function hashPassword(password) {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    console.log(`Original: ${password}`);
-    console.log(`Hashed: ${hashedPassword}`);
+    return hashedPassword;
 }
 
-
-hashPassword('adminpassword123');
+hashPassword('12345').then(hashedPassword => {
+    console.log('Hashed password:', hashedPassword);
+});
